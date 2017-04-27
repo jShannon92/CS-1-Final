51a9fd4f5710ca2beca8a10a4ee1345841d8487f
D'Metri Cortez <djc.metri@gmail.com>
2017-04-27 16:14:51 -0700

    Snake Movement
    Coded snake movement and changed framerate

--- code/main.cpp
+++ code/main.cpp
@@ -1,10 +1,11 @@
 /*
-Snake Game v0.0.6
+Snake Game v0.0.7
 04/27/2017
 */
 /* CHANGE LOG:
-v0.0.6
-- Added enum function to give directions a numerical value (line 81)
+v0.0.7
+- changed some names of objects
+- created the input and logic functions to allow snake movement
 */
 
 #include <iostream> 
@@ -49,10 +50,10 @@ void displayMap(){
 		std::cout << "[]";
 		
 		for(int i2=0; i2<tiles.size(); i2++){
-			if(headXPos == i && headYPos == i2){ //If the current cell being rendered is the same coordinates as the head.
-				std:: cout << "OO";
+			if(headYPos == i && headXPos == i2){ //If the current cell being rendered is the same coordinates as the head.
+				std:: cout << " O";              //Switched the headYPos and headXPos since they were backwards and changing the X value would move it along the Y axis
 			}else if(foodXPos == i && foodYPos == i2){ //Current cell is where the food is.
-				std::cout << "**";
+				std::cout << " *";
 			}else if(tiles[i][i2].getMoveable() == false){ //Unmoveable space. We actually probably don't need this unless we want to make obsticales in the middle of the map.
 				std::cout << "[]";
 			}else if(tiles[i][i2].getMoveable() == true){ //Empty space.
@@ -78,14 +79,54 @@ void displayMap(){
 }
 
 //Checks for key presses and returns a number 1-4 depending on which key pressed.
-enum keyChecker { UP, DOWN, LEFT, RIGHT} snake; // auto gives UP, DOWN, LEFT, and RIGHT the values 0-3
-		/*std::cout << keyChecker(UP) << std::endl;
-		std::cout << keyChecker(DOWN) << std::endl;
-		std::cout << keyChecker(LEFT) << std::endl;
-		std::cout << keyChecker(RIGHT) << std::endl;
-		Checked this out and it prints out 0 - 3 like it should */
-		
+enum direction { UP, DOWN, LEFT, RIGHT} snake;
+	direction dir;
 
+
+ // auto gives UP, DOWN, LEFT, and RIGHT the values 0-3
+		/*std::cout << sdirection(UP) << std::endl;
+		std::cout << sdirection(DOWN) << std::endl;
+		std::cout << sdirection(LEFT) << std::endl;
+		std::cout << sdirection(RIGHT) << std::endl;
+		Checked this out and it prints out 0 - 3 like it should */
+void input(){ // grabs the input w a s d and gives them a direction
+	if (_kbhit())
+	{
+		switch (_getch())
+		{
+			case 'w':
+				dir = UP;
+				break;
+			case 's':
+				dir = DOWN;
+				break;
+			case 'a':
+				dir = LEFT;
+				break;
+			case 'd':
+				dir = RIGHT;
+				break;
+		}
+	}
+}		
+void logic(){                 // HELL YEAH GOT THIS TO WORK
+	switch (dir)
+	{
+		case UP:
+			headYPos--;
+			break;
+		case DOWN:
+			headYPos++;
+			break;
+		case LEFT:
+			headXPos--;
+			break;
+		case RIGHT:
+			headXPos++;
+			break;
+			
+	}
+}
 //Generates random coordinates for a new food object. Checks to make sure it's not in a position where a head or tail piece is.
 void randomFoodGenerator(){
 	// ???
@@ -118,9 +159,11 @@ int main(){
 	//The main game loop.
 	while(gameOver == false){ //Keep looping this until the game is over / player loses.
 		displayMap();
-		Sleep(400); //Pause the program for a fraction of a second to prevent the game from running too fast.
+		input();
+		logic();
+		Sleep(140); //Pause the program for a fraction of a second to prevent the game from running too fast.
 		
-		int keyReturn = keyChecker();
+		int keyReturn = direction();
 		if(keyReturn != 0){ //Check to see if the keyReturn changed because of a key press.
 			moveDirection = keyReturn; //Set the moveDirection to the key return int.
 			keyReturn = 0; //Return keyReturn to it's default state.
