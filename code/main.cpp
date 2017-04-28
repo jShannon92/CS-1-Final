409116c46ecf639f894be05d98e573d9194ec9ba
jShannon92 <jacob.r.shannon@gmail.com>
2017-04-27 21:38:39 -0700

    v0.0.8
    - Added the start screen
    - Modified the debug info shown above the map.
    - Removed some old unused code.
    - Fixed the food displaying on the wrong coordinates.
    - Added wall and food collision
    - Added score display.

--- code/main.cpp
+++ code/main.cpp
@@ -1,11 +1,15 @@
 /*
-Snake Game v0.0.7
+Snake Game v0.0.8
 04/27/2017
 */
 /* CHANGE LOG:
-v0.0.7
-- changed some names of objects
-- created the input and logic functions to allow snake movement
+v0.0.8
+- Added the start screen
+- Modified the debug info shown above the map.
+- Removed some old unused code.
+- Fixed the food displaying on the wrong coordinates.
+- Added wall and food collision
+- Added score display.
 */
 
 #include <iostream> 
@@ -13,12 +17,13 @@ v0.0.7
 #include "Windows.h"
 #include "MapTile.h"
 #include <conio.h>
+#include <sstream> // Needed to convert int to string
+
+HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE); //Handle used for changing text ouput color.
 
 std::vector< std::vector <MapTile> > tiles;
 
 bool gameOver = false; //Boolean that causes the main game loop to end after the game is over.
-
-int moveDirection; //Integer that tells which direction the snake is moving towards.
 
 int headXPos = 4; //4 being a random starting position for now.
 int headYPos = 4; //4 being a random starting position for now.
@@ -31,52 +36,45 @@ int highscore = 0;
 
 //vector for tail cordinates?
 
-void displayMap(){
-	system("cls");
-	std::cout << "MapSize x: " << tiles.size() << std::endl;
-	std::cout << "MapSize y: " << tiles[0].size() << std::endl;
-	std::cout << "headXPos: " << headXPos << std::endl;
-	std::cout << "headYPos: " << headYPos << std::endl;
-	std::cout << "foodXPos: " << foodXPos << std::endl;
-	std::cout << "foodYPos: " << foodYPos << std::endl;
-	std::cout << "moveDirection = " << moveDirection << std::endl;
-	//Top Border
-	for(int b=0; b<tiles[0].size()+2; b++){
-		std::cout << "[]";
+/* 
+	Adds an appropriate amount of padding to the left and right of a string to make it appear centered in a column.
+	If the string is too large for the text field it will cut off the end with an ellipses.
+	@param string – A string.
+	@param fieldsize – An integer.
+	@return – A string the length of fieldsize containing the input string with spaces preceding and following it.
+*/ 	
+std::string centerString(std::string text, int fieldsize){
+	if(text.length() == fieldsize){
+		//No centering needs to be done because it fits perfectly.
+		return text;
 	}
-	std::cout << std::endl;
-	for(int i=0; i<tiles[0].size(); i++){	
-		//Left Border	
-		std::cout << "[]";
-		
-		for(int i2=0; i2<tiles.size(); i2++){
-			if(headYPos == i && headXPos == i2){ //If the current cell being rendered is the same coordinates as the head.
-				std:: cout << " O";              //Switched the headYPos and headXPos since they were backwards and changing the X value would move it along the Y axis
-			}else if(foodXPos == i && foodYPos == i2){ //Current cell is where the food is.
-				std::cout << " *";
-			}else if(tiles[i][i2].getMoveable() == false){ //Unmoveable space. We actually probably don't need this unless we want to make obsticales in the middle of the map.
-				std::cout << "[]";
-			}else if(tiles[i][i2].getMoveable() == true){ //Empty space.
-				std::cout << "  ";
-			/*
-			}else if(  ~~Check to see if part of the tail is here~~ ){
-				std::cout << "~~";
-			*/
-			}else{ //SOMETHING WENT WRONG!
-				std::cout << "??"; 
-			}
-			
-		}
-		//Right Border
-		std::cout << "[]" << std::endl;
+	if(text.length() > fieldsize){
+		//If the text is larger than the fieldsize, trim the length down to the fieldsize - 3 and add an ellipses to the end.
+		text.erase(text.length() - ((text.length() - fieldsize) + 3), ((text.length() - fieldsize) + 3));
+		text += "...";
+		return text;
 	}
-	//Bottom Border
-	for(int b=0; b<tiles[0].size()+2; b++){
-		std::cout << "[]";
+	int leftpadding;
+	int rightpadding;
+	leftpadding = (fieldsize - text.length()) / 2;
+	//Check to see if the fieldsize is even.
+	if (fieldsize % 2 == 0){
+		//If the text length is an odd amount and the field size is even then the rightpadding will have one less space due to a lost remainder.
+		if(text.length() % 2 == 0){
+			rightpadding = (fieldsize - text.length()) / 2;
+		}else{
+			rightpadding = (fieldsize - text.length()) / 2 + 1;
+		} 
+	}else {
+		if(text.length() % 2 == 0){
+			rightpadding = (fieldsize - text.length()) / 2 + 1;
+		}else{
+			rightpadding = (fieldsize - text.length()) / 2;
+		} 
 	}
-	std::cout << std::endl;
-	
-}
+	return std::string(leftpadding,' ') + text + std::string(rightpadding,' '); 
+};
+
 
 //Checks for key presses and returns a number 1-4 depending on which key pressed.
 enum direction { UP, DOWN, LEFT, RIGHT} snake;
@@ -127,6 +125,147 @@ void logic(){                 // HELL YEAH GOT THIS TO WORK
 			
 	}
 }
+
+void displayMap(){
+	system("cls");
+	std::cout << "MapSize x: " << tiles.size() << std::endl;
+	std::cout << "MapSize y: " << tiles[0].size() << std::endl;
+	std::cout << "headXPos: " << headXPos << std::endl;
+	std::cout << "headYPos: " << headYPos << std::endl;
+	std::cout << "foodXPos: " << foodXPos << std::endl;
+	std::cout << "foodYPos: " << foodYPos << std::endl;
+	std::cout << "dir = " << dir << std::endl;
+	std::cout << "score = " << score << std::endl;
+	//Top Border
+	for(int b=0; b<tiles[0].size()+2; b++){
+		std::cout << "[]";
+	}
+	std::cout << std::endl;
+	for(int i=0; i<tiles[0].size(); i++){	
+		//Left Border	
+		std::cout << "[]";
+		
+		for(int i2=0; i2<tiles.size(); i2++){
+			if(headYPos == i && headXPos == i2){ //If the current cell being rendered is the same coordinates as the head.
+				std:: cout << " O";              //Switched the headYPos and headXPos since they were backwards and changing the X value would move it along the Y axis
+			}else if(foodYPos == i && foodXPos == i2){ //Current cell is where the food is.
+				std::cout << " *";
+			}else if(tiles[i][i2].getMoveable() == false){ //Unmoveable space. We actually probably don't need this unless we want to make obsticales in the middle of the map.
+				std::cout << "[]";
+			}else if(tiles[i][i2].getMoveable() == true){ //Empty space.
+				std::cout << "  ";
+			/*
+			}else if(  ~~Check to see if part of the tail is here~~ ){
+				std::cout << "~~";
+			*/
+			}else{ //SOMETHING WENT WRONG!
+				std::cout << "??"; 
+			}
+			
+		}
+		//Right Border
+		std::cout << "[]" << std::endl;
+	}
+	//Bottom Border
+	for(int b=0; b<tiles[0].size()+2; b++){
+		std::cout << "[]";
+	}
+	std::cout << std::endl << std::endl;
+	
+	//Convert the points int to a string.
+	std::ostringstream scoreStringStream;
+    scoreStringStream << score;
+    std::string scoreString = scoreStringStream.str();
+
+	std::cout << centerString(scoreString,(tiles[0].size()+2)*2) << std::endl; //Display the score at the bottom of the screen.
+};
+
+void startingScreen(){
+	int leftBorder = 5;
+	//Length: 46
+	std::cout << std::endl << std::endl;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << std::string( leftBorder, ' ' );
+	SetConsoleTextAttribute(hConsole, 15); std::cout << "  "; SetConsoleTextAttribute(hConsole, 32); std::cout << "      "; 
+	SetConsoleTextAttribute(hConsole, 15); std::cout << "  "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "  " ;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << "    "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "  " ;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << "    "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "   " ;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << "    "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "  " ;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << "    "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "  " ;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << " "; SetConsoleTextAttribute(hConsole, 32); std::cout << "        " << std::endl;
+	SetConsoleTextAttribute(hConsole, 15);std::cout << std::string( leftBorder, ' ' );
+	SetConsoleTextAttribute(hConsole, 15); std::cout << " "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "  " ;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << "    "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "  " ;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << " "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "   " ;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << "   "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "  " ;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << "   "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "  " ;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << " "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "  " ;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << "   "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "  " ;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << "   "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "  " ;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << "  "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "  " << std::endl;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << std::string( leftBorder, ' ' );
+	SetConsoleTextAttribute(hConsole, 15); std::cout << " "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "  " ;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << "       "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "    " ;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << "  "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "  " ;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << "  "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "  " ;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << "   "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "  " ;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << "  "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "  " ;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << "  "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "  " ;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << "   "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "  " << std::endl;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << std::string( leftBorder, ' ' );
+	SetConsoleTextAttribute(hConsole, 15); std::cout << "  "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "      " ;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << "  "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "  " ;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << " "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "  " ;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << " "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "  " ;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << " "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "  " ;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << "     "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "  " ;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << " "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "     " ;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << "    "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "      " ;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << "  "; SetConsoleTextAttribute(hConsole, 32); std::cout  << std::endl;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << std::string( leftBorder, ' ' );
+	SetConsoleTextAttribute(hConsole, 15); std::cout << "       "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "  " ;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << " "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "  " ;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << "  "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "    " ;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << " "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "         " ;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << " "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "  " ;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << "  "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "  " ;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << "   "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "  " << std::endl;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << std::string( leftBorder, ' ' );
+	SetConsoleTextAttribute(hConsole, 15); std::cout << " "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "  " ;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << "    "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "  " ;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << " "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "  " ;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << "   "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "   " ;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << " "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "  " ;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << "     "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "  " ;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << " "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "  " ;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << "   "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "  " ;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << "  "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "  " << std::endl;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << std::string( leftBorder, ' ' );
+	SetConsoleTextAttribute(hConsole, 15); std::cout <<  "  "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "      " ;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << "  "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "  " ;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << "    "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "  " ;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << " "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "  " ;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << "     "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "  " ;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << " "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "  " ;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << "    "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "  " ;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << " "; SetConsoleTextAttribute(hConsole, 32); std::cout  << "        " << std::endl;
+	SetConsoleTextAttribute(hConsole, 15); std::cout << std::endl << std::endl;
+	
+	std::cout << centerString("By:",46+leftBorder*2) << std::endl;
+	std::cout << centerString("D'Metri Cortez",46+leftBorder*2) << std::endl;
+	std::cout << centerString("Jacob Shannon",46+leftBorder*2) << std::endl;
+		std::cout << centerString("Brittany Green",46+leftBorder*2) << std::endl;
+	std::cout << std::endl << std::endl;
+	std::cout << centerString("Press [Enter] to start.",46+leftBorder*2) << std::endl;
+
+	bool enterPress = false;
+	while(enterPress == false){
+		char key = _getch();
+		if(key == '\r'){ //If the enter key is pressed
+			enterPress = true;
+		}
+	}
+};
+
 //Generates random coordinates for a new food object. Checks to make sure it's not in a position where a head or tail piece is.
 void randomFoodGenerator(){
 	// ???
@@ -155,23 +294,20 @@ int main(){
 	for(int i=0; i<tiles.size(); i++){
 		tiles[i].resize(10);
 	}
-	
+	startingScreen();
 	//The main game loop.
 	while(gameOver == false){ //Keep looping this until the game is over / player loses.
 		displayMap();
 		input();
 		logic();
 		Sleep(140); //Pause the program for a fraction of a second to prevent the game from running too fast.
-		
-		int keyReturn = direction();
-		if(keyReturn != 0){ //Check to see if the keyReturn changed because of a key press.
-			moveDirection = keyReturn; //Set the moveDirection to the key return int.
-			keyReturn = 0; //Return keyReturn to it's default state.
+		if(headXPos < 0 || headYPos < 0 || headYPos >= tiles[0].size() || headXPos >= tiles.size()){ //Head hit a wall.
+			gameOver = true;
 		}
-		
-		// ~~If statements for each direction.~~
-	
-		// ~~If statement for wall collision.~~
+		if(headXPos == foodXPos && headYPos == foodYPos){ //Head hit some food. 
+			score++;
+		}
+		// ~~If statement for tail collision.~~
 		
 		//.....
 		
